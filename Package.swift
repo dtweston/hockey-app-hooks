8b07ca01267f5bb4496427c84ef9f123082aa2b5
Dave Weston <dweston@yammer-inc.com>
2017-05-30 13:13:45 -0700

    Update SwiftDotEnv and commit pins file

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
 		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
 		.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
 		.Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0),
-		.Package(url: "https://github.com/SwiftOnTheServer/SwiftDotEnv.git", majorVersion: 0)
+		.Package(url: "https://github.com/SwiftOnTheServer/SwiftDotEnv.git", majorVersion: 1)
     ]
 )
